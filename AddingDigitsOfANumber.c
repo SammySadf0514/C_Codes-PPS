ad38a8384acc671d07a870885e8f3d9f2d83b364
SammySadf0514 <sameersadar@gmail.com>
2024-03-19 10:33:05 +0530

    C Beginners Code

--- AddingDigitsOfANumber.c
+++ AddingDigitsOfANumber.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int i,j,k,sum;
+    int i,j,k;
+    int sum = 0;
     printf("Enter the number: ");
     scanf("%d",&i);
     while(i>0){
-    j=i%10;
-    sum=(sum)+j;
-    i=i/10;
+        j=i%10;
+        sum=sum+j;
+        i=i/10;
     }
-    printf("Reverse number: %d",sum);
+    printf("Sum of the number: %d",sum);
     return 0;
 }
